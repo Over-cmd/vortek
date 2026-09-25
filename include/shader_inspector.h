4e92dfdddcbd6c2769f536e9feb21b170380a42d
Over-cmd <sergioleonpines@gmail.com>
2026-09-25 10:55:50 +0200

    Create shader_inspector.h for SPIR-V compatibility
    Added header file for shader inspector with Vulkan support.

---
+++ include/shader_inspector.h
@@ -0,0 +1,8 @@
+#ifndef SHADER_INSPECTOR_H
+#define SHADER_INSPECTOR_H
+
+#include <vulkan/vulkan.h>
+
+/* 🚨 BLINDAJE VORTEK: Puente de compatibilidad para el inspector de Shaders SPIR-V */
+
+#endif // SHADER_INSPECTOR_H
