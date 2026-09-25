c936a7d469779da05ba4f915e30c016ff2ab483a
Over-cmd <sergioleonpines@gmail.com>
2026-09-25 10:56:29 +0200

    Create format_emulation.h for texture format support
    Added header file for texture format emulation compatibility.

---
+++ include/format_emulation.h
@@ -0,0 +1,8 @@
+#ifndef FORMAT_EMULATION_H
+#define FORMAT_EMULATION_H
+
+#include <vulkan/vulkan.h>
+
+/* 🚨 BLINDAJE VORTEK: Puente de compatibilidad para la emulación de formatos de textura */
+
+#endif // FORMAT_EMULATION_H
