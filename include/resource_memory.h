ce57d73dd6c7d73641de27ee4fa91f93baae92b4
Over-cmd <sergioleonpines@gmail.com>
2026-09-25 10:47:29 +0200

    Add resource_memory.h header file

---
+++ include/resource_memory.h
@@ -0,0 +1,10 @@
+#ifndef RESOURCE_MEMORY_H
+#define RESOURCE_MEMORY_H
+
+#include <vulkan/vulkan.h>
+
+/* 🚨 CABECERA DE COMPATIBILIDAD VORTEK:
+   Satisface el preprocesador de Clang para que no colapse la build por 
+   archivos huérfanos omitidos en el repositorio original. */
+
+#endif // RESOURCE_MEMORY_H
