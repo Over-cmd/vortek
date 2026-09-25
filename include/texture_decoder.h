bf7336c536a80db16c281a2dad785ea86354f9f4
Over-cmd <sergioleonpines@gmail.com>
2026-09-25 10:57:00 +0200

    Add texture_decoder.h header file

---
+++ include/texture_decoder.h
@@ -0,0 +1,8 @@
+#ifndef TEXTURE_DECODER_H
+#define TEXTURE_DECODER_H
+
+#include <vulkan/vulkan.h>
+
+/* 🚨 BLINDAJE VORTEK: Puente de compatibilidad para la decodificación asíncrona */
+
+#endif // TEXTURE_DECODER_H
